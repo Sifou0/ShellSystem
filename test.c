90489f7de93b61549864b4681e36ea72bbcc61fb
Sifou0 <92372046+Sifou0@users.noreply.github.com>
2022-06-13 19:03:09 +0200

    up to date

--- test.c
+++ test.c
@@ -8,7 +8,8 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <dirent.h>
-
+#include <sys/stat.h>
+#include <fcntl.h>
 void cd(char* path)
 {
   int d = chdir(path);
@@ -17,12 +18,37 @@ void cd(char* path)
 
 int main()
 {
-    char* buf[2];
-    buf[0] = "ls";
-    buf[1] = NULL; 
-    char cwd[100];
-    cd("testfff");
-    getcwd(cwd,100);
-    printf("%s\n",cwd);
+    char* s = "bash < test.txt";
+    char* start = (char*) malloc(strlen(s) * sizeof(char));
+    char* delim = "<";
+    int in = 0;
+    int out = 0;
+    while(*s)
+    {
+      start[strlen(start)] = *s;
+      if(*s == '<')
+      {
+        in = 1;
+        break;
+      }
+      s++;
+    }
+    start[strlen(start)- 1 ] = '\0';
+    if(start[strlen(start)- 1 ] == ' ') start[strlen(start)- 1 ] = '\0';
+    if(in) s++;
+    if(s[0] == ' ') s++;
+    if(s[0] > 40 && s[0] < 127)
+    {
+      int opened_file = open(s,0,O_RDONLY);
+      if(opened_file)
+      {
+        char buf[4096];
+        read(opened_file,buf,sizeof(buf));
+        printf("%s\n",buf);
+      }
+      close(opened_file);
+    }
+    else printf("fffff\n");
+    printf("%s\n",start);
 	return 0;
 }
